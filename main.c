9f654b08dff86c90d9af6828de5e9deee27a533e
xmif1 <xandru.mifsud@gmail.com>
2021-04-02 08:14:57 +0200

    Minor changes.

--- main.c
+++ main.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
+#include <pthread.h>
 #include "client_server.h"
+
+void* server_listen(void* arg);
 
 int main(){
     int socket_fd = client_init();
-    int ret = send_msg("test", socket_fd);
 
-    char* msg;
-    dequeue(msg);
+    pthread_t server_thread;
+    if(socket_fd >= 0){
+        if(pthread_create(&server_thread, NULL, server_listen, (void*) &socket_fd) != 0){
+            return -1;
+        }
+        else{
+            int ret = send_msg("test", socket_fd);
+            char* msg;
+            dequeue_msgs(msg);
+
+            printf("%s\n", msg);
+        }
+    }
 
     return 0;
+}
+
+void* server_listen(void* arg){
+    int socket_fd = *((int*) arg);
+    enqueue_msgs(socket_fd);
 }
