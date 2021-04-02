d3f1ee84d152ae72395964839fba7223151360df
Xandru Mifsud <xandru.mifsud@gmail.com>
2021-04-02 11:14:49 +0200

    Fixing race condition.

--- main.c
+++ main.c
@@ -2,29 +2,16 @@
 #include <pthread.h>
 #include "client_server.h"
 
-void* server_listen(void* arg);
-
 int main(){
     int socket_fd = client_init();
 
-    pthread_t server_thread;
     if(socket_fd >= 0){
-        if(pthread_create(&server_thread, NULL, server_listen, (void*) &socket_fd) != 0){
-            return -1;
-        }
-        else{
-            int ret = send_msg("test", socket_fd);
-            char* msg;
-            dequeue_msgs(msg);
+       int ret = send_msg("test", socket_fd);
+       enqueue_msgs(socket_fd);
+       char* msg = dequeue_msgs();
 
-            printf("%s\n", msg);
-        }
+       printf("%s\n", msg);
     }
 
-    return 0;
-}
-
-void* server_listen(void* arg){
-    int socket_fd = *((int*) arg);
-    enqueue_msgs(socket_fd);
+  return 0;
 }
